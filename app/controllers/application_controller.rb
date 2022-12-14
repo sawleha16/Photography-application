3f06a0248d24127fa0f51f3d595a431ebef700d6
Sawleha shaikh <sawlehashaikh161@gmail.com>
2022-12-14 17:41:21 +0530

    changes has been made

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action :confirgure_permitted_parameters,if: :devise_controller?
+  before_action :confirgure_permitted_parameters, if: :devise_controller?
 
   protected
+
   def confirgure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email,:password,:password_confirmation,:username,:age,:interests)}
+    if action_name == 'create'
+      devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email, :password, :password_confirmation, :username, :age,:interests)}
+    elsif action_name == 'update'
+     devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:email, :password, :password_confirmation, :current_password, :username, :age, :interests)}
+   end
   end
 end
