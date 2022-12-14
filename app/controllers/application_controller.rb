830f6d6865636fe86b1661054944e38e60ee091e
Sawleha shaikh <sawlehashaikh161@gmail.com>
2022-12-14 12:33:04 +0530

    Add custom attir to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :confirgure_permitted_parameters,if: :devise_controller?
+
+  protected
+  def confirgure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email,:password,:password_confirmation,:username,:age,:interests)}
+  end
 end
