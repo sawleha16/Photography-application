fae86a8932b6d904ec7b16d7a2e0d44fee2ee664
Ahmad hamza <ahmadhamza19@gmail.com>
2023-01-10 20:09:46 +0530

    user validation spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :trackable
-  INTEREST_FIELD =['Wedding', 'Events', 'Potraits']
+  INTEREST_FIELD = ['Wedding', 'Events', 'Potraits']
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
-require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    context 'username' do
+      specify do
+        u = User.new(email: 'abc@gmail.com', password: '1234test', age: 24, username: nil)
+        expect(u.valid?).to eq(false)
+        expect(u.errors.messages).to eq({username: ["can't be blank"]})
+      end
+    end
+
+    context 'age' do
+      specify do
+        u = User.new(email: 'abc@gmail.com', password: '1234test', age: nil, username: 'abc')
+        expect(u.valid?).to eq(false)
+        expect(u.errors.messages).to eq({age: ["can't be blank"]})
+      end
+    end
+  end
 end
