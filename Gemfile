f6596ec5a7f53b778d29a3d53cc70a2b110e57f9
Sawleha shaikh <sawlehashaikh161@gmail.com>
2023-01-11 21:13:13 +0530

    Add feature test for album index pg

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem "sqlite3", "~> 1.4"
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
-gem 'pg'
+# gem 'pg'
 
 # Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
 gem "importmap-rails"
@@ -53,7 +53,7 @@ gem "bootsnap", require: false
 
 group :development, :test do
   gem 'rspec-rails'
-  gem 'factory_bot_rails', '~> 6.2'  
+  gem 'factory_bot_rails', '~> 6.2'
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
 end
---
+++ spec/features/album_spec.rb
@@ -0,0 +1,42 @@
+RSpec.describe 'index page', type: :feature do
+  def user_login_form(email, password)
+    within('form#new_user') do
+      fill_in 'Email', with: 'abc@gmail.com'
+      fill_in 'Password', with: '1234test'
+      find('input[type=submit]').click
+      visit'/'
+    end
+  end
+
+  context 'when the user is logged in' do
+    before do
+      create(:user, email: 'abc@gmail.com')
+    end
+
+   specify do
+     visit root_path
+     expect(page).to have_current_path('/users/sign_in')
+     user_login_form('abc@gmail.com', '1234test')
+     expect(page).to have_current_path(root_path)
+     expect(page).to have_text('Welcome to 500px')
+     expect(page).to have_css('#albums', exact_text: '')
+   end
+ end
+
+ context 'when there is an album' do
+   let(:user) { create(:user, email: 'abc@gmail.com')}
+   let(:album) { create(:album, name: 'abcd', description: 'abcdefg', user_id: user.id)}
+    before do
+      album
+    end
+
+    specify do
+      visit root_path
+      expect(page).to have_current_path('/users/sign_in')
+      user_login_form('abc@gmail.com', '1234test')
+      expect(page).to have_current_path(root_path)
+      expect(page).to have_text('Welcome to 500px')
+      expect(page).to have_css('#albums', exact_text: 'abcd')
+    end
+  end
+end
